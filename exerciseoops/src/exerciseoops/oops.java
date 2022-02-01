dee5d472e544b685139c0e9a8f98a9f7b281e60c
JAAGO INDIA JAAGO <JAAGO INDIA JAAGO@JAAGOINDIAJAAGO>
2022-02-01 14:29:46 +0530

    goodone

---
+++ exerciseoops/src/exerciseoops/oops.java
@@ -0,0 +1,17 @@
+package exerciseoops;
+
+public class oops {
+
+	public static void main(String[] args) {
+		/*
+	question:
+		 create a class game, which allows a user to play "guess the number"
+		 game once. game should have the following methods:
+		 1. Constructor to generate the random number
+		 2. takeUserInput() to take a user input of number
+		 3. isCorrectNumber() to detect whether the number entered by the user is true
+		 4. getter and setter for noOfGuesses
+		 Use properties such as noOfGuesses(int), etc to get this task done!
+		 */
+	}
+}
